8a91d4ce8c05371d5d0022e933ebd0ea15d3f4d0
Ifeoluwapo123 <65716233+Ifeoluwapo123@users.noreply.github.com>
2020-07-21 02:09:32 -0700

    Update location.php

--- location.php
+++ location.php
@@ -3,13 +3,10 @@
 	$db = new loc();
 
 	if(isset($_POST['lat1']) && isset($_POST['lng1'])){
-		$db->updateUserCurrentLocation($_POST['lat1'],
-									   $_POST['lng1']);
+		$db->updateUserCurrentLocation($_POST['lat1'],$_POST['lng1']);
 		echo "Location successfully updated";
 	}else if(isset($_POST['lat2']) && isset($_POST['lng2']) && isset($_POST['res'])){
-		$db->updateUserDestinationLocation($_POST['res'],
-										   $_POST['lat2'],
-										   $_POST['lng2']);
+		$db->updateUserDestinationLocation($_POST['res'],$_POST['lat2'],$_POST['lng2']);
 		echo "Location Recorded";
 	}
 ?>
